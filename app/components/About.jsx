dc90af7853028d56c672b6b46c9baaa49b12f7d6
Harsh6575 <harsh.190410107146@gmail.com>
2023-06-09 17:13:59 +0530

    projects and about page added

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,11 +1,110 @@
 "use client";
-import React from 'react';
-import {SectionWrapper} from '../hoc';
+import React, { useEffect } from "react";
+import { SectionWrapper } from "../hoc";
+import { styles } from "../styles";
+import { groq } from "next-sanity";
+import { client } from "../../sanity/lib/client";
+
+const query = groq`*[_type == "about"] {
+  _createdAt,
+    title,
+    body[]{
+      children[]{
+    ...,
+    text,
+      marks[]
+      }
+    }
+} | order(_createdAt asc)`;
 
 const About = () => {
+  const [paragraphs, setParagraphs] = React.useState([]);
+
+  useEffect(() => {
+    const fetchAbout = async () => {
+      try {
+        const response = await client.fetch(query);
+
+        if (response.length === 0) {
+          throw new Error("No data found");
+        }
+
+        const paragraphs = response.map((about) => {
+          const markClassMap = {
+            strong: "text-myPurple",
+            underline: "text-myGreen",
+            "strike-through": "text-myBlue",
+            em: "text-myRed",
+            code: "text-myEbony",
+            highlight: "text-myViolet",
+          };
+
+          const descriptionElements = about.body[0].children.map((child) => {
+            let className = "";
+
+            if (child._type === "span" && child.marks.length > 0) {
+              className = child.marks
+                .map((mark) => markClassMap[mark])
+                .filter((className) => !!className)
+                .join(" ");
+            }
+
+            return (
+              <span key={child._key} className={className}>
+                {child.text}
+              </span>
+            );
+          });
+
+          let currentParagraph = [];
+          const generatedParagraphs = [];
+
+          descriptionElements.forEach((element) => {
+            if (element.text === "\n") {
+              generatedParagraphs.push(
+                <p key={generatedParagraphs.length}>{currentParagraph}</p>
+              );
+              currentParagraph = [];
+            } else {
+              currentParagraph.push(element);
+            }
+          });
+
+          if (currentParagraph.length > 0) {
+            generatedParagraphs.push(
+              <p key={generatedParagraphs.length}>{currentParagraph}</p>
+            );
+          }
+
+          return generatedParagraphs;
+        });
+
+        setParagraphs(paragraphs);
+      } catch (error) {
+        console.error("Error fetching navigation links:", error);
+      }
+    };
+
+    setInterval(fetchAbout, 1000);
+
+    fetchAbout();
+  }, [paragraphs]);
+  
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <div>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </div>
+      <div className="mt-20 flex flex-wrap gap-10">
+        {paragraphs.map((paragraph, index) => (
+          <div key={index} className="flex flex-col gap-4">
+            {paragraph}
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default SectionWrapper(About, "about");
--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -1,11 +1,122 @@
 "use client";
-import React from 'react';
-import {SectionWrapper} from '../hoc';
+import React from "react";
+import { SectionWrapper } from "../hoc";
+import { Tilt } from "react-tilt";
+
+import Image from "next/image";
+
+import { styles } from "../styles";
+import { groq } from "next-sanity";
+import { client } from "../../sanity/lib/client";
+import { AiFillEye, AiOutlineGithub } from "react-icons/ai";
+import { urlForImage } from "../../sanity/lib/image";
+
+const query = groq`*[_type == "project"] | order(_createdAt asc)`;
+
+const ProjectCard = ({
+  index,
+  name,
+  description,
+  tags,
+  image,
+  source_code_link,
+  live_demo_link,
+}) => {
+  image = urlForImage(image).url();
+
+  return (
+    <Tilt
+      options={{
+        max: 45,
+        scale: 1,
+        speed: 450,
+      }}
+      className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+    >
+      <div className="relative w-full h-[230px]">
+        <img
+          src={image}
+          alt={name}
+          className="w-full h-full object-cover rounded-2xl"
+        />
+
+        <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+          {source_code_link && (
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <AiOutlineGithub className="w-1/2 h-1/2 object-contain" />
+            </div>
+          )}
+          {live_demo_link && (
+            <div
+              onClick={() => window.open(live_demo_link, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <AiFillEye className="w-1/2 h-1/2 object-contain" />
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="mt-5">
+        <h3 className="text-white font-bold text-[24px]">{name}</h3>
+        <p className="mt-2 text-secondary text-[14px]">{description}</p>
+      </div>
+      {tags && (
+        <div className="mt-4 flex flex-wrap gap-2">
+          {tags.map((tag) => (
+            <p
+              key={`${name}-${tag.name}`}
+              className={`text-[14px] ${tag.color}`}
+            >
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+      )}
+    </Tilt>
+  );
+};
 
 const Projects = () => {
+  const [projects, setProjects] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const response = await client.fetch(query);
+
+        if (response.length === 0) {
+          throw new Error("No data found");
+        }
+
+        setProjects(response);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+
+
+    fetchProjects();
+  }, [projects]);
+
+  console.log(projects);
+
   return (
-    <div>Projects</div>
-  )
-}
+    <>
+      <div>
+        <p className={styles.sectionSubText}>Here are some of my</p>
+        <h2 className={styles.sectionHeadText}>Projects.</h2>
+      </div>
+      <div className="mt-20 flex flex-wrap gap-10">
+        {projects.map((project, index) => (
+          <ProjectCard key={project.name} index={index} {...project} />
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default SectionWrapper(Projects, "projects");
